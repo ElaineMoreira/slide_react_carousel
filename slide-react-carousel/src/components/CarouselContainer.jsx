2d53f189eabeb779c986224d8c9f8450e8c6d13d
Elaine <elainefelipemoreira@gmail.com>
2022-02-22 10:00:44 -0300

    finaliza a apresentacao

--- slide-react-carousel/src/components/CarouselContainer.jsx
+++ slide-react-carousel/src/components/CarouselContainer.jsx
@@ -8,84 +8,70 @@ const CarouselContainer = () => {
   <Carousel.Item interval={2000}>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image01.png?raw=true"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
   <Carousel.Item interval={2000}>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image02.png?raw=true"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
   <Carousel.Item interval={1000}>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image03.png?raw=true"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>Tird slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image04.png?raw=true"
       alt="Third slide"
     />
     <Carousel.Caption>
-      <h3>Quarto slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
   <Carousel.Item interval={2000}>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image05.png?raw=true"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>Quinto slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   
   <Carousel.Item interval={2000}>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image06.png?raw=true"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>Sexto slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
   <Carousel.Item interval={2000}>
     <img
       className="d-block w-100"
-      src="https://mocah.org/mocahbig/80787-ubuntu-linux-computer-hd-4k.jpg"
+      src="https://github.com/ElaineMoreira/slide_react_carousel/blob/main/slide-react-carousel/src/images/image07.png?raw=true"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>Setimo slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
 
